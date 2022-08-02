dda58169a6de5db7afec2c9c09b2452b8ef9ca6d
Vasinc <lein0.jade@gmail.com>
2022-08-03 01:24:58 +0300

    3 ( header fixed(sort of) )

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,15 +1,21 @@
 const burgerMenu = document.querySelector('.burger-menu');
 const links = document.querySelector('.links');
 const backdrop = document.querySelector('.backdrop');
+const header = document.querySelector('header');
 
 function removeClassesHandler() {
     links.classList.remove('right-zero');
     backdrop.classList.remove('display-block');
+    header.classList.remove('overflow-visible');
+    document.body.style.overflow = 'visible';
 }
 
 burgerMenu.addEventListener('click', () => {
+    header.classList.add('overflow-visible');
     links.classList.add('right-zero');
     backdrop.classList.add('display-block');
+    document.body.style.overflow = 'hidden';
+    header.scrollIntoView();
 })
 
 backdrop.addEventListener('click', removeClassesHandler);
