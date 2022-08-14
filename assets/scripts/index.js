d0780c8be4b34a45bc6a757174595138125ce98b
Vasinc <lein0.jade@gmail.com>
2022-08-15 00:03:04 +0300

    5 ( main about added )

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,8 +2,45 @@ const burgerMenu = document.querySelector('.burger-menu');
 const links = document.querySelector('.links');
 const backdrop = document.querySelector('.backdrop');
 const header = document.querySelector('header');
+const deformablePhoto = document.getElementById('deformable-picture');
+const deformText = document.getElementById('text-after-deform');
 
-// idk
+let borderRadiusData = [0, 0 ,0 ,0];
+let clicksLeft = 5;
+let clickable = true;
+
+function deformPhotoHandler(event) {
+    if (!clickable) return;
+    const photo = event.target;
+    if (clicksLeft == 1) {
+        clickable = false;
+        photo.style.display = 'none';
+        deformText.style.display = 'block';
+    }
+    const rndNum = Math.floor(Math.random() * 5 + 10)
+    const rndCorner = Math.floor(Math.random() * 4);
+    console.log(rndCorner);
+    switch (rndCorner) {
+        case 0:
+            photo.style.borderTopLeftRadius = `${borderRadiusData[0] + rndNum}rem`
+            borderRadiusData[0] += rndNum;
+            break;
+        case 1:
+            photo.style.borderTopRightRadius = `${borderRadiusData[1] + rndNum}rem`
+            borderRadiusData[1] += rndNum;
+            break;
+        case 2:
+            photo.style.borderBottomLeftRadius = `${borderRadiusData[2] + rndNum}rem`
+            borderRadiusData[2] += rndNum;
+            break;
+        case 3:
+            photo.style.borderBottomRightRadius = `${borderRadiusData[3] + rndNum}rem`
+            borderRadiusData[3] += rndNum;
+            break;
+    }
+    clicksLeft--;
+    console.log(borderRadiusData);
+}
 
 function removeClassesHandler() {
     links.classList.remove('right-zero');
@@ -27,3 +64,5 @@ window.addEventListener('resize', () => {
         removeClassesHandler();
     }
 })
+
+deformablePhoto.addEventListener('click', deformPhotoHandler);
