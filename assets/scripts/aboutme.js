5edbedfac65d548f5927747f043b6533453d5b37
Vasinc <lein0.jade@gmail.com>
2022-08-28 21:18:17 +0300

    10 ( about me page )

---
+++ assets/scripts/aboutme.js
@@ -0,0 +1,6 @@
+let typed = new Typed(".dynamic-text", {
+    strings: ["coder.", "gym rat.", "open-minded person.", "lazy gentleman ;)"],
+    typeSpeed: 100,
+    backSpeed: 50,
+    loop: true
+})
